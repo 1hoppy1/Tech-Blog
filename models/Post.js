adeaa180458e52b25ceed0e3d9cebc9e8a9bcc31
Jim Hopkinson <jahopki@hotmail.com>
2020-08-07 13:25:53 -0600

    adding routes and file structure

--- models/Post.js
+++ models/Post.js
@@ -1,43 +1,44 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+    Model,
+    DataTypes
+} = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
 class Post extends Model {}
 
 // create fields/columns for Post model
-Post.init(
-    {
-      id: {
+Post.init({
+    id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
-      },
-      title: {
+    },
+    title: {
         type: DataTypes.STRING,
         allowNull: false
-      },
-      post_url: {
+    },
+    post_url: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          isURL: true
+            isURL: true
         }
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      }
-    },
-    {
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'post'
     }
-  );
+    // ,
+    // user_id: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //         model: 'user',
+    //         key: 'id'
+    //     }
+    // }
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+});
 
-  module.exports = Post;
+module.exports = Post;
---
+++ routes/api/post-routes.js
@@ -0,0 +1,174 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      // Query configuration
+    })
+  
+  });
+
+  const router = require('express').Router();
+const {
+  Post,
+  User,
+  Vote
+} = require('../../models');
+const sequelize = require('../../config/connection');
+
+
+// get all users
+router.get('/', (req, res) => {
+  Post.findAll({
+      attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']]
+      // ,
+      // include: [
+      //   {
+      //     model: User,
+      //     attributes: ['username']
+      //   }
+      // ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ]
+      // ,
+      // include: [
+      //   {
+      //     model: User,
+      //     attributes: ['username']
+      //   }
+      // ]
+    })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: 'No post found with this id'
+        });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST /api/posts
+router.post('/', (req, res) => {
+  // expects {title: 'Forrest Gump', post_url: "https://www.imdb.com/title/tt0109830/?ref_=nv_sr_srsg_0"}
+  Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  // create the vote
+  Vote.create({
+      user_id: req.body.user_id,
+      post_id: req.body.post_id
+    })
+    // new problem below
+    .then(() => {
+      // then find the post we just voted on
+      return Post.findOne({
+          where: {
+            id: req.body.post_id
+          },
+          attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+            [
+              sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+              'vote_count'
+            ]
+          ]
+        })
+
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+
+      //end brackets below  
+    })
+});
+
+
+
+router.put('/:id', (req, res) => {
+
+  Post.update(req.body, {
+      // individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({
+          message: 'No movie found with this id'
+        });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({
+          message: 'No movie found with this id'
+        });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
