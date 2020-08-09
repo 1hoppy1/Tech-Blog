71d6fa62409c5faf3a423befd6ef9fbe7cfab737
Jim Hopkinson <jahopki@hotmail.com>
2020-08-08 18:03:32 -0600

    before

--- server.js
+++ server.js
@@ -4,15 +4,33 @@ const sequelize = require('./config/connection');
 const path = require('path');
 
 
+//add in session cookies
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+//add in session cookies
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//chpt14
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-//chpt14
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
